f7cbfc1bed123ba5c6efacc5c6000dbcac9a32d5
Abhinav Maharana <abhinavmaharana.004@gmail.com>
2022-01-29 16:55:41 +0530

    ✅ Added footer and contact

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import About from '../components/HomePage/About/About';
+import Contact from '../components/HomePage/Contact/Contact';
 import DetailsSection from '../components/HomePage/DetailsSection/DetailsSection';
 import Features from '../components/HomePage/Features/Features';
+import Footer from '../components/HomePage/Footer/Footer';
 import Header from '../components/HomePage/Header/Header';
 import HomeSection from '../components/HomePage/HomeSection/HomeSection';
 import Team from '../components/HomePage/Team/Team';
@@ -15,6 +17,8 @@ const Home = () => {
         <DetailsSection />
         <Features />
         <Team />
+        <Contact />
+        <Footer />
       </>
   );
 };
