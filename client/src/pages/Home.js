5f8dc424d24dc101bb9345ccde04c1e9204e6f48
Abhinav Maharana <abhinavmaharana.004@gmail.com>
2022-01-29 14:12:20 +0530

    ✅ Added Home Section

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../components/HomePage/Header/Header';
+import HomeSection from '../components/HomePage/HomeSection/HomeSection';
 
 const Home = () => {
   return (
       <>
         <Header />
+        <HomeSection />
       </>
   );
 };
