19a14338deacff6ad6e740af7f1125bfe7a807a5
Abhinav Maharana <abhinavmaharana.004@gmail.com>
2022-01-29 13:46:51 +0530

    ✅ Added Header

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Header from '../components/HomePage/Header/Header';
 
 const Home = () => {
   return (
       <>
-        <h1>Hello</h1>
+        <Header />
       </>
   );
 };
